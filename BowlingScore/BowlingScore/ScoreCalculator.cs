c0e9f97acfbf8b61e4f6982e37dd4b881f793cf6
Msatkin <mscottaa@gmail.com>
2016-11-03 12:03:25 -0500

    Rewrote 50% of code, fixing all known bugs

--- BowlingScore/BowlingScore/ScoreCalculator.cs
+++ BowlingScore/BowlingScore/ScoreCalculator.cs
@@ -58,30 +58,52 @@ namespace BowlingScore
         {
             for (int i = 0; i < frames.Length; i++)
             {
-                if (i == 9)
+                if (i < 7)
                 {
-                    scorePerFrame[i] = GetFrameTenScore(i);
+                    GetNormalFrameScore(i);
                 }
-                else if (frames[i] == "X")
+                else if (i == 7)
                 {
-                    scorePerFrame[i] = 10 + GetNextTwo(i + 1);
+                    GetEigthFrame();
                 }
-                else if (frames[i][1] == '/')
+                else if (i == 8)
                 {
-                    scorePerFrame[i] = 10 + GetNextOne(i + 1);
+                    GetNinthFrame();
                 }
-                else
+                else if (i == 9)
                 {
-                    int rollOneScore;
-                    int rollTwoScore;
-                    string rollOne = frames[i][0].ToString();
-                    string rollTwo = frames[i][1].ToString();
-
-                    int.TryParse(rollOne, out rollOneScore);
-                    int.TryParse(rollTwo, out rollTwoScore);
-                    scorePerFrame[i] = rollOneScore + rollTwoScore;
+                    GetTenthFrame();
                 }
             }
+        }
+        private void GetNormalFrameScore(int frame)
+        {
+            if (frames[frame][0] == 'X')
+            {
+                scorePerFrame[frame] = 10 + GetNextTwo(frame + 1);
+            }
+            else if (frames[frame][1] == '/')
+            {
+                scorePerFrame[frame] = 10 + GetNextOne(frame + 1);
+            }
+            else
+            {
+                scorePerFrame[frame] = GetFirstRollScore(frame) + GetSecondRollScore(frame);
+            }
+        }
+        private int GetFirstRollScore(int frame)
+        {
+            int roll;
+            string rollOne = frames[frame][0].ToString();
+            int.TryParse(rollOne, out roll);
+            return roll;
+        }
+        private int GetSecondRollScore(int frame)
+        {
+            int roll;
+            string rollTwo = frames[frame][1].ToString();
+            int.TryParse(rollTwo, out roll);
+            return roll;
         }
         private int GetNextTwo(int frame)
         {
@@ -91,11 +113,7 @@ namespace BowlingScore
             }
             else if (frames[frame][0] == 'X')
             {
-                int score = 10;
-                int rollOneScore;
-                string rollOne = frames[frame + 1][0].ToString();
-                int.TryParse(rollOne, out rollOneScore);
-                return score + rollOneScore;
+                return 10 + GetFirstRollScore(frame + 1);
             }
             else if (frames[frame][1] == '/')
             {
@@ -103,14 +121,7 @@ namespace BowlingScore
             }
             else
             {
-                int rollOneScore;
-                int rollTwoScore;
-                string rollOne = frames[frame][0].ToString();
-                string rollTwo = frames[frame][1].ToString();
-
-                int.TryParse(rollOne, out rollOneScore);
-                int.TryParse(rollTwo, out rollTwoScore);
-                return rollOneScore + rollTwoScore;
+                return GetFirstRollScore(frame) + GetSecondRollScore(frame);
             }
         }
         private int GetNextOne(int frame)
@@ -121,50 +132,94 @@ namespace BowlingScore
             }
             else
             {
-                int rollOneScore;
-                string rollOne = frames[frame][0].ToString();
-                int.TryParse(rollOne, out rollOneScore);
-                return rollOneScore;
+                return GetFirstRollScore(frame);
             }
         }
-        private int GetFrameTenScore(int frame)
+        private void GetEigthFrame()
         {
-            int score = 0;
-            if (frames[frame][0] == 'X' && frames[frame][1] == 'X' && frames[frame][2] == 'X')
+            if (frames[7][0] == 'X' && frames[8][0] == 'X')
             {
-                return 30;
+                scorePerFrame[7] = 20 + GetTenthFrameValue(0);
             }
-            else if (frames[frame][1] == '/')
+            else if (frames[7][0] == 'X' && frames[8][1] == '/')
             {
-                score = 10;
-                if (frames[frame][1] == 'X')
-                {
-                    score += 10;
-                }
-                else
-                {
-                    int rollOneScore;
-                    string rollOne = frames[frame][2].ToString();
-                    int.TryParse(rollOne, out rollOneScore);
-                    score += rollOneScore;
-                }
-                return score;
+                scorePerFrame[7] = 20;
+            }
+            else if (frames[7][0] == 'X')
+            {
+                scorePerFrame[7] = 10 + GetFirstRollScore(7) + GetSecondRollScore(7);
+            }
+            else if (frames[7][1] == '/')
+            {
+                scorePerFrame[7] = 10 + GetFirstRollScore(8);
             }
             else
             {
-                int rollOneScore;
-                int rollTwoScore;
-                string rollOne = frames[frame][0].ToString();
-                string rollTwo = frames[frame][1].ToString();
-
-                int.TryParse(rollOne, out rollOneScore);
-                int.TryParse(rollTwo, out rollTwoScore);
-                return rollOneScore + rollTwoScore;
+                scorePerFrame[7] = GetFirstRollScore(7) + GetSecondRollScore(7);
             }
         }
-        private int GetFrameTenNextRoll()
+        private void GetNinthFrame()
         {
-            return 10;
+            if (frames[8][0] == 'X' && frames[9][0] == 'X')
+            {
+                scorePerFrame[8] = 20 + GetTenthFrameValue(1);
+            }
+            else if (frames[8][0] == 'X' && frames[9][1] == '/')
+            {
+                scorePerFrame[8] = 20;
+            }
+            else if (frames[8][0] == 'X')
+            {
+                scorePerFrame[8] = 10 + GetTenthFrameValue(0) + GetTenthFrameValue(1);
+            }
+            else if (frames[8][1] == '/')
+            {
+                scorePerFrame[8] = 10 + GetTenthFrameValue(0);
+            }
+            else
+            {
+                scorePerFrame[8] = GetFirstRollScore(8) + GetSecondRollScore(8);
+            }
+        }
+        private void GetTenthFrame()
+        {
+            if (frames[9].Length == 3)
+            {
+                if (frames[9][0] == 'X' && frames[9][1] == 'X')
+                {
+                    scorePerFrame[9] = 20 + GetTenthFrameValue(2);
+                }
+                else if (frames[9][0] == 'X' && frames[9][2] == '/')
+                {
+                    scorePerFrame[9] = 20;
+                }
+                else if (frames[9][0] == 'X')
+                {
+                    scorePerFrame[9] = 10 + GetTenthFrameValue(1) + GetTenthFrameValue(2);
+                }
+                else
+                {
+                    scorePerFrame[9] = 10 + GetTenthFrameValue(2);
+                }
+            }
+            else
+            {
+                scorePerFrame[9] = GetTenthFrameValue(0) + GetTenthFrameValue(1);
+            }
+        }
+        private int GetTenthFrameValue(int index)
+        {
+            if (frames[9][index] == 'X')
+            {
+                return 10;
+            }
+            else
+            {
+                int roll;
+                string frameTen = frames[9][index].ToString();
+                int.TryParse(frameTen, out roll);
+                return roll;
+            }
         }
     }
 }
